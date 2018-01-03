39a142913f5f2911723f38a13d0376cc5db9e987
Chris Winfrey <christopher.winfrey@endurance.com>
2018-01-02 18:42:40 -0700

    Additional sections

--- app/src/components/forecast/forecast.jsx
+++ app/src/components/forecast/forecast.jsx
@@ -9,8 +9,39 @@ class Forecast extends Component {
     return (
       <div>
         <div className="row">
-          <h3 className="text-info font-weight-normal my-2" style={{marginLeft: "15px"}}>{this.props.geo.city}</h3>
+          <h3 className="text-info font-weight-light my-2" style={{marginLeft: "15px"}}>{this.props.geo.city}</h3>
         </div>
+        <div className="row">
+          <div className="col">
+            <div className="card bg-dark text-white">
+              <div className="card-header"></div>
+              <div className="card-body">
+                <p>test</p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card bg-dark text-white">
+              <div className="card-header"></div>
+              <div className="card-body">
+                <p>test</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row mt-5">
+          <h4 className="text-white font-weight-light my-2" style={{marginLeft: "15px"}}>Next Few Hours</h4>
+        </div>
+        {/*hourly forecast*/}
+
+        <div className="row mt-5">
+          <h4 className="text-white font-weight-light my-2" style={{marginLeft: "15px"}}>
+            This Week's Forecast <small className="text-info hidden-xs-down">Info</small>
+          </h4>
+        </div>
+        {/*Weekly Forecasr*/}
+
       </div>
     )
   }
