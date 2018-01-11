f9572f0a1edc3fb2eac26c7002102c74dac4832d
Chris Winfrey <christopher.winfrey@endurance.com>
2018-01-11 11:50:02 -0700

    Further updates

--- app/src/components/forecast/forecast.jsx
+++ app/src/components/forecast/forecast.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import Skycons from 'react-skycons';
-import moment from 'moment';
+import moment from 'moment-timezone';
 import numeral from 'numeral';
-
-;
 
 class Forecast extends Component {
 
@@ -12,12 +10,21 @@ class Forecast extends Component {
     let icon = name.replace(/-/g, "_");
     icon = icon.toUpperCase();
     return (
-
       <Skycons color="white" icon={icon} />
     )
   }
 
+  hourlyForecast(days) {
+
+  }
+
   render () {
+
+    let current = this.props.darkSky.currently;
+    let daily = this.props.darkSky.daily;
+    let today = this.props.darkSky.daily.data[0];
+    let minutely = this.props.darkSky.minutely;
+
     console.log(this.props);
     return (
       <div>
@@ -30,24 +37,26 @@ class Forecast extends Component {
               <div className="card-header">
                 <h3 className="font-weight-normal"><strong>Current Conditions </strong><small className="text-warning hidden-xs-down">{moment().format("hh:mm a")}</small></h3>
               </div>
-              <div className="card-body">
+              <div className="card-body forecast">
                 <div className="row">
                   <div className="col-md-4 p-0 d-inline-block">
-                    {this.getIcon(this.props.darkSky.currently.icon)}
-                  </div>
-                  <div className="col-md-8">
-                    <div className="temp">
-                    {`${numeral(this.props.darkSky.currently.temperature).format('00')}º`}
+                    <div className="mt-4">
+                      {this.getIcon(current.icon)}
                     </div>
-                    <strong> {this.props.darkSky.currently.summary}</strong>
+                  </div>
+                  <div className="col-md-8 d-inline-block">
+                    <div className="temp">
+                    {`${numeral(current.temperature).format('00')}º`}
+                    </div>
+                    <strong> {current.summary}</strong>
                     <br /> 
-                    {this.props.darkSky.minutely.summary}
+                    {minutely.summary}
                     <br />
-                    {`Humidity: ${numeral(this.props.darkSky.currently.humidity).format('0')}%`} | {`Dew Point: ${numeral(this.props.darkSky.currently.dewPoint).format('00')}º`}
+                    {`Humidity: ${numeral(current.humidity).format('0%')}`} | {`Dew Point: ${numeral(current.dewPoint).format('00')}º`}
                     <br />
-                    {`Cloud Cover: ${numeral(this.props.darkSky.currently.cloudCover).format('0')}%`} | {`Pressure: ${numeral(this.props.darkSky.currently.pressure).format('00.00')}`}
+                    {`Cloud Cover: ${numeral(current.cloudCover).format('0%')}`} | {`Pressure: ${numeral(current.pressure).format('00.00')}`}
                     <br />
-                    {`Wind Speed: ${numeral(this.props.darkSky.currently.windSpeed).format('0.0')} mph`} | {`Visibility: ${this.props.darkSky.currently.visibility} mi`}
+                    {`Wind Speed: ${numeral(current.windSpeed).format('0.0')} mph`} | {`Visibility: ${current.visibility} mi`}
                   </div>
                 </div>
               </div>
@@ -58,7 +67,38 @@ class Forecast extends Component {
               <div className="card-header">
                 <h3 className="font-weight-normal"><strong>Today's Forecast </strong><small className="text-warning">{moment().format("dddd, MMM Do")}</small></h3>
               </div>
-              <div className="card-body">
+              <div className="card-body forecast">
+                <div className="row">
+                  <div className="col-md-4 p-0 d-inline-block">
+                    <div className="mt-4">
+                      {this.getIcon(daily.icon)}
+                    </div>
+                  </div>
+                  <div className="col-md-8 d-inline-block">
+                    <div className="text-primary inline font30 pr-3">
+                      <strong>{`${numeral(today.temperatureHigh).format('00')}º`}</strong>
+                    </div>
+                    <div className="text-info inline font30">
+                      <strong>{`${numeral(today.temperatureLow).format('00')}º`}</strong>
+                    </div>
+                    <br />
+                    <p className="mb-0">
+                      <strong>{today.summary}</strong>
+                    </p>
+                    <p className="mb-0">
+                      <small>{`Humidity: ${numeral(today.humidity).format('0%')}`} </small>
+                        |
+                      <small> {`Chance of Rain: ${numeral(today.precipProbability).format('0%')}`} </small>
+                        |
+                      <small> Wind: {(today.windSpeed < 1) ? 1 : `${numeral(today.windSpeed).format('0.0')}`} mph</small>
+                    </p>
+                    <p>
+                      <small>Sunrise: {moment(today.sunriseTime * 1000).tz(this.props.darkSky.timezone).format('hh:mm a')} </small>
+                        |
+                      <small> Sunset: {moment(today.sunsetTime * 1000).tz(this.props.darkSky.timezone).format('hh:mm a')}</small>
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -68,10 +108,10 @@ class Forecast extends Component {
           <h4 className="text-white font-weight-light my-2" style={{marginLeft: "15px"}}>Next Few Hours</h4>
         </div>
         {/*hourly forecast*/}
-
+        {this.hourlyForecast(daily.data)}
         <div className="row mt-5">
           <h4 className="text-white font-weight-light my-2" style={{marginLeft: "15px"}}>
-            This Week's Forecast <small className="text-info hidden-xs-down">Info</small>
+            This Week's Forecast | <small className="text-info hidden-xs-down">{daily.summary}</small>
           </h4>
         </div>
         {/*Weekly Forecasr*/}
